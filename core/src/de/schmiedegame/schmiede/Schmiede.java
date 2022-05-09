0dcc8a323412a307b12875aa1859b05b62d34284
DerKekk <87085389+DerKekk@users.noreply.github.com>
2022-05-09 18:07:53 +0200

    Add files via upload

---
+++ core/src/de/schmiedegame/schmiede/Schmiede.java
@@ -0,0 +1,21 @@
+package de.schmiedegame.schmiede;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+public class Schmiede extends ApplicationAdapter {
+
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
